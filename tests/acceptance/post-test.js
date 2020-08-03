a40a7162b7e92085bce88e2d6e88341ff3352f8b
LC <lrc3233@gmail.com>
2020-08-02 23:09:07 -0700

    updated acceptance test

--- tests/acceptance/post-test.js
+++ tests/acceptance/post-test.js
@@ -1,6 +1,7 @@
 import setupAcceptanceTest from '../helpers/setup-acceptance-test';
 import {module, test} from 'qunit';
-import {visit, currentURL, find} from '@ember/test-helpers';
+import {visit, currentURL, find, waitUntil, settled} from '@ember/test-helpers';
+import {defer} from 'rsvp';
 
 module('Acceptance | post', function (hooks) {
   setupAcceptanceTest(hooks);
@@ -11,7 +12,25 @@ module('Acceptance | post', function (hooks) {
   });
 
   test('visiting /post', async function (assert) {
-    await visit('/post/1');
+    const deferred = defer();
+    let wasCommentQueryCalled = false;
+
+    this.server.get('comments', async (schema, request) => {
+      wasCommentQueryCalled = true;
+      await deferred.promise;
+
+      const post = schema.posts.find(request.queryParams.post_id);
+      return post.comments;
+    });
+
+    // Do not await
+    visit('/post/1');
+
+    await waitUntil(() => wasCommentQueryCalled);
+    assert.ok(find('[data-test-comments-loading]'), 'comments should show loading state');
+
+    await deferred.resolve();
+    await settled();
 
     assert.equal(currentURL(), '/post/1');
     assert.ok(find('[data-test-post-body]'), 'post body should be present');
