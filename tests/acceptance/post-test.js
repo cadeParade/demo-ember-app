7adb6db2459003f66603ee59c7c4a9cb9c9819ac
LC <lrc3233@gmail.com>
2020-08-11 21:26:12 -0700

    after video

--- tests/acceptance/post-test.js
+++ tests/acceptance/post-test.js
@@ -1,6 +1,6 @@
 import setupAcceptanceTest from '../helpers/setup-acceptance-test';
 import {module, test} from 'qunit';
-import {visit, currentURL, find} from '@ember/test-helpers';
+import {visit, currentURL, find, waitUntil, settled} from '@ember/test-helpers';
 
 module('Acceptance | post', function (hooks) {
   setupAcceptanceTest(hooks);
@@ -11,7 +11,12 @@ module('Acceptance | post', function (hooks) {
   });
 
   test('visiting /post', async function (assert) {
-    await visit('/post/1');
+    visit('/post/1');
+
+    await waitUntil(() => find('[data-test-comments-loading]'));
+    assert.ok(find('[data-test-comments-loading]'), 'comment loader should be present');
+
+    await settled();
 
     assert.equal(currentURL(), '/post/1');
     assert.ok(find('[data-test-post-body]'), 'post body should be present');
